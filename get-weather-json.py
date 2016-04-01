292e4f7c7eba8b1427411306b5f8315b9192e6c6
matthewf01 <matthewfugel@gmail.com>
2016-04-01 19:48:04 -0400

    Update get-weather-json.py

--- get-weather-json.py
+++ get-weather-json.py
@@ -4,8 +4,8 @@ import requests
 import urllib
 
 #define my variables
-weather_city="GA"
-weather_state="Suwanee"
+weather_city="Suwanee"
+weather_state="GA"
 weather_conditions_url = "http://api.wunderground.com/api/c11da38a0c7b7fc3/conditions/q/%s/%s.json" % (weather_state,weather_city)
 weather_working_dir = "/home/pi/Documents/pi-weather-clock/weatherconditions.json"
 
