4bde239471a588d0f5fa1f566bcfdc8924688772
matthewf01 <matthewfugel@gmail.com>
2016-04-02 14:19:27 -0400

    Update get-weather-json.py

--- get-weather-json.py
+++ get-weather-json.py
@@ -7,12 +7,17 @@ import time
 #define my variables
 weather_city="Suwanee"
 weather_state="GA"
-weather_conditions_url = "http://api.wunderground.com/api/c11da38a0c7b7fc3/conditions/q/%s/%s.json" % (weather_state,weather_city)
+weather_conditions_url = "http://api.wunderground.com/api/c11da38a0c7b7fc3/conditions/q/{}/{}.json".format(weather_state,weather_city)
 weather_working_dir = "/home/pi/Documents/pi-weather-clock/weatherconditions.json"
 
 def urlget(): 
+  timenow = time.strftime("%X")
+  datenow = time.strftime("%x")
   urllib.urlretrieve(weather_conditions_url, weather_working_dir)
+  print("{} | {} || Weather Condition JSON retrieved for {}/{}".format(datenow, timenow, weather_state,weather_city))
   time.sleep(900)
 
 while True:
   urlget()
+
+
