26c69ec5a576e511ad2ced1979c33b7fcf0bb508
matthewf01 <matthewfugel@gmail.com>
2016-04-06 18:39:58 -0400

    Update news_player.py

--- news_player.py
+++ news_player.py
@@ -41,43 +41,36 @@ print "Current working directory %s" % retval
 
 
 while True:
- '''
-    request=raw_input("RGB-->")
-    if (len(request) == 3):
-     GPIO.output(16,int(request[0]))
-     GPIO.output(20,int(request[1]))
-     GPIO.output(21,int(request[2]))
- '''
-    if (GPIO.input(5) == True):
-        #os.system('mpg123 -q sound_file_zelda &')
-        print ("Green pressed")
-        GPIO.output(16,1)
-        GPIO.output(20,0)
-        GPIO.output(21,1)
-        time.sleep(1)
+  if (GPIO.input(5) == True):
+   #os.system('mpg123 -q sound_file_zelda &')
+   print ("Green pressed")
+   GPIO.output(16,1)
+   GPIO.output(20,0)
+   GPIO.output(21,1)
+   time.sleep(1)
  
-    if (GPIO.input(6) == True):
-        #os.system('mpg123 -q Zelda_Theme.mp3 &')
-        print ("Red pressed")
-        GPIO.output(16,1)
-        GPIO.output(20,1)
-        GPIO.output(21,0)
-        time.sleep(1)
+  if (GPIO.input(6) == True):
+   #os.system('mpg123 -q Zelda_Theme.mp3 &')
+   print ("Red pressed")
+   GPIO.output(16,1)
+   GPIO.output(20,1)
+   GPIO.output(21,0)
+   time.sleep(1)
  
-    if (GPIO.input(13)== True):
-        #os.system('mpg123 -q Zelda_Theme.mp3 &')
-        print ("Yellow pressed")
-        GPIO.output(16,1)
-        GPIO.output(20,0)
-        GPIO.output(21,0)
-        time.sleep(1)
-        
-    if (GPIO.input(19)== True):
-        #os.system('mpg123 -q Zelda_Theme.mp3 &')
-        print ("Blue pressed")
-        GPIO.output(16,0)
-        GPIO.output(20,1)
-        GPIO.output(21,1)
-        time.sleep(1)
+  if (GPIO.input(13)== True):
+   #os.system('mpg123 -q Zelda_Theme.mp3 &')
+   print ("Yellow pressed")
+   GPIO.output(16,1)
+   GPIO.output(20,0)
+   GPIO.output(21,0)
+   time.sleep(1)
+   
+  if (GPIO.input(19)== True):
+   #os.system('mpg123 -q Zelda_Theme.mp3 &')
+   print ("Blue pressed")
+   GPIO.output(16,0)
+   GPIO.output(20,1)
+   GPIO.output(21,1)
+   time.sleep(1)
  
-    sleep(0.1);
+  sleep(0.1);
