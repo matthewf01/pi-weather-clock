1d294e466959f8a31c9a15d164ba006f861982d5
matthewf01 <matthewfugel@gmail.com>
2016-04-06 14:43:19 -0400

    Update get-weather-json.py

--- get-weather-json.py
+++ get-weather-json.py
@@ -10,15 +10,16 @@ weather_state="GA"
 weather_conditions_url = "http://api.wunderground.com/api/c11da38a0c7b7fc3/conditions/q/{}/{}.json".format(weather_state,weather_city)
 weather_working_dir = "/home/pi/Documents/pi-weather-clock/weatherconditions.json"
 
-def urlget(): 
-  timenow = time.strftime("%X")
-  datenow = time.strftime("%x")
-  #UNCOMMENT THIS WHEN READY TO PULL NEW WEATHER DATA; COMMENTED OUT FOR TESTING
-  urllib.urlretrieve(weather_conditions_url, weather_working_dir)
-  print("{} | {} || Weather Condition JSON retrieved for {}/{}".format(datenow, timenow, weather_state,weather_city))
-  time.sleep(600)
+def urlget():
+   while (True):
+     timenow = time.strftime("%X")
+     datenow = time.strftime("%x")
+     #UNCOMMENT THIS WHEN READY TO PULL NEW WEATHER DATA; COMMENTED OUT FOR TESTING
+    urllib.urlretrieve(weather_conditions_url, weather_working_dir)
+    print("{} | {} || Weather Condition JSON retrieved for {}/{}".format(datenow, timenow, weather_state,weather_city))
+    time.sleep(600)
 
-while True:
+while (True):
   urlget()
 
 
