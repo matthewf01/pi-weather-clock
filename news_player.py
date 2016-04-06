c402f5e46d87b25aacd8ec99acfecb6039f611e0
matthewf01 <matthewfugel@gmail.com>
2016-04-06 18:15:56 -0400

    Update news_player.py

--- news_player.py
+++ news_player.py
@@ -5,12 +5,20 @@ import os
 from time import sleep
  
 import RPi.GPIO as GPIO
- 
+
+#buttons 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.IN)
 GPIO.setup(6, GPIO.IN)
 GPIO.setup(13, GPIO.IN)
 GPIO.setup(19, GPIO.IN)
+#LEDs
+GPIO.setup(16,GPIO.OUT)
+GPIO.output(16,0)
+GPIO.setup(20,GPIO.OUT)
+GPIO.output(20,0)
+GPIO.setup(21,GPIO.OUT)
+GPIO.output(21,0)
 
 sound_file_path = "/home/pi/Music/test"
 sound_file_zelda ="Zelda_Theme.mp3"
@@ -22,6 +30,13 @@ retval = os.getcwd()
 print "Current working directory %s" % retval
 
 while True:
+    request=raw_input(RGB-->")
+    if (len(request) == 3):
+     GPIO.output(16,int(request[0]))
+     GPIO.output(20,int(request[1]))
+     GPIO.output(21,int(request[2]))
+     
+ '''
     if (GPIO.input(5) == True):
         os.system('mpg123 -q sound_file_zelda &')
         print ("Green pressed")
@@ -43,3 +58,4 @@ while True:
         time.sleep(1)
  
     sleep(0.1);
+'''
