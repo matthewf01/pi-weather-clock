82efab42787ee5bec002a4c14cf15ad9033a8183
matthewf01 <matthewfugel@gmail.com>
2016-04-06 11:53:15 -0400

    Update news_player.py

--- news_player.py
+++ news_player.py
@@ -27,15 +27,15 @@ while True:
         print ("Green pressed")
  
     if (GPIO.input(6) == True):
-        os.system('mpg123 -q Zelda_Theme.mp3 &')
+        #os.system('mpg123 -q Zelda_Theme.mp3 &')
         print ("Red pressed")
  
     if (GPIO.input(13)== True):
-        os.system('mpg123 -q Zelda_Theme.mp3 &')
+        #os.system('mpg123 -q Zelda_Theme.mp3 &')
         print ("Yellow pressed")
         
     if (GPIO.input(19)== True):
-        os.system('mpg123 -q Zelda_Theme.mp3 &')
+        #os.system('mpg123 -q Zelda_Theme.mp3 &')
         print ("Blue pressed")
  
-    sleep(1);
+    sleep(0.1);
