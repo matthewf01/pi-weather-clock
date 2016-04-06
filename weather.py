67da2d100781dd21d8897ffd47051b7313480fb5
matthewf01 <matthewfugel@gmail.com>
2016-04-06 12:04:34 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -98,7 +98,6 @@ def main():
    read_json_conditions()
    LCD_ready()
    lcd.set_cursor(0,1)
-   lcd.autoscroll(True)
    lcd.message("{}, {}{}F".format(weather,tempf,chr(223)))
    break
   else:
